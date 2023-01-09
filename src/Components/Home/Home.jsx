f9d6968f2e4071a23b1a4e6882794e47a7b18cba
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2023-01-09 11:57:32 +0100

    add_something

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,6 +6,7 @@ const Home = ()=>{
         <div className="Home">
             <Container>
                 <h1>Home Component</h1>
+                <p>All You need To know, just search</p>
             </Container>
         </div>
     )
